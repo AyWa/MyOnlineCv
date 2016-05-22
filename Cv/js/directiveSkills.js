e40091068e291ee7bfab5be6f21dffa32605e384
HMarc <marc.hurabielle@gmail.com>
2016-05-22 17:46:10 +0200

    skills & skill directive

---
+++ Cv/js/directiveSkills.js
@@ -0,0 +1,27 @@
+var app = angular.module('mydirectiveSkill', []);
+app.directive('mySkills',['myfactory',function(myfactory){
+  return{
+    restrict: 'E',
+    scope:{
+    },
+    templateUrl: 'template/skills.html',
+    controller:function($scope){
+      console.log("hello skills");
+    }
+  }
+}]);
+app.directive('mySkill',['myfactory',function(myfactory){
+  return{
+    restrict: 'E',
+    scope:{
+      name:'@',
+      pourcentage:'@',
+    },
+    templateUrl: 'template/skill.html',
+    controller:function($scope){
+      console.log("hello skill");
+      if($scope.name==='C++') $scope.logo="/image/skill/cplusplus.png";
+      if($scope.name==='AngularJs') $scope.logo="/image/skill/angular.png"
+    }
+  }
+}]);
--- Cv/js/myapps.js
+++ Cv/js/myapps.js
@@ -1,1 +1,1 @@
-var myapp = angular.module('myappCv', ['mydirectiveExp','mydirectiveTop']);
+var myapp = angular.module('myappCv', ['mydirectiveExp','mydirectiveTop','mydirectiveSkill']);
