8970de5f80ff4a494adeb4da449f29db24217b2d
HMarc <marc.hurabielle@gmail.com>
2016-05-23 21:11:08 +0200

    animation on scroll

--- Cv/js/directiveHobbies.js
+++ Cv/js/directiveHobbies.js
@@ -1,4 +1,4 @@
-var app = angular.module('mydirectiveHobbies', []);
+var app = angular.module('mydirectiveHobbies', ['angular-scroll-animate']);
 app.directive('myHobbies',['myfactory',function(myfactory){
   return{
     restrict: 'E',
@@ -8,6 +8,16 @@ app.directive('myHobbies',['myfactory',function(myfactory){
     controller:function($scope){
       $scope.hobbies=myfactory.gotHobbies();
       console.log('hello Hobbies');
+
+      $scope.animateElementIn = function($el) {
+        $el.removeClass('hidden');
+        $el.addClass('animated fadeInUp');
+      };
+      // optional: not mandatory (uses angular-scroll-animate)
+      $scope.animateElementOut = function($el) {
+        $el.addClass('hidden');
+        $el.removeClass('animated fadeInUp');
+      };
     }
   }
 }]);
--- Cv/js/directiveSkills.js
+++ Cv/js/directiveSkills.js
@@ -9,7 +9,7 @@ app.directive('mySkills',['myfactory',function(myfactory){
       $scope.listSkills=myfactory.gotListSkill();
       console.log("hello skills");
       console.log($scope.listSkills);
-    }
+    },
   }
 }]);
 app.directive('mySkill',['myfactory',function(myfactory){
@@ -23,23 +23,44 @@ app.directive('mySkill',['myfactory',function(myfactory){
     templateUrl: 'template/skill.html',
     controller:function($scope,$interval){
       $scope.pourcentage_anim=0;
+      var Myannim;
       console.log("hello skill");
       if($scope.name==='C++') $scope.logo="/image/skill/cplusplus.png";
       if($scope.name==='AngularJs') $scope.logo="/image/skill/angular.png";
       if($scope.name==='Français') $scope.logo="/image/flagFr.png";
       if($scope.name==='Anglais') $scope.logo="/image/flagUk.png";
       if($scope.name==='Coréen') $scope.logo="/image/flagKr.png";
-      $scope.animload = $interval(function(){
+      /*$scope.animload = $interval(function(){
          $scope.pourcentage_anim+=2;
          if($scope.pourcentage_anim>=$scope.pourcentage) $interval.cancel($scope.animload);
-      },30);
+      },30);*/
+      $scope.animateElementIn = function($el) {
+        $el.removeClass('hidden');
+        if($scope.pourcentage_anim<$scope.pourcentage) $scope.startanimload();
+      };
+      // optional: not mandatory (uses angular-scroll-animate)
+      $scope.animateElementOut = function($el) {
+        $el.addClass('hidden');
+        $scope.pourcentage_anim=0;
+        if (angular.isDefined(Myannim)) {
+          $interval.cancel(Myannim);
+          Myannim = undefined;
+        }
+      };
+      $scope.startanimload = function(){
+        if ( angular.isDefined(Myannim) ) return;
+        Myannim = $interval(function(){
+          $scope.pourcentage_anim+=2;
+          if ($scope.pourcentage_anim>=$scope.pourcentage) {
+            if (angular.isDefined(Myannim)) {
+              $interval.cancel(Myannim);
+              Myannim = undefined;
+            }
+          }
+        }, 30);
+      };
     },
     link: function(scope,elem,attrs){
-      /*console.log(elem);
-      console.log(attrs);
-      scope.$watch(function() { return elem.is(':visible') }, function() {
-          console.log(scope.name);
-      }); */
     }
   }
 }]);
