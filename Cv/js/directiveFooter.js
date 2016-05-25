e4c68b1e3e8f950c9c9fcc4ff5e11d9e21df9568
HMarc <marc.hurabielle@gmail.com>
2016-05-26 00:23:15 +0200

    footer modif with service + add linkedin to service

--- Cv/js/directiveFooter.js
+++ Cv/js/directiveFooter.js
@@ -5,6 +5,8 @@ app.directive('myFooter',['myfactory',function(myfactory){
     templateUrl: 'template/footer.html',
     controller:function($scope){
       console.log("hello footer");
+      $scope.myPers=myfactory.gotPers();
+      console.log($scope.myPers.email);
     },
     link:function(scope,element,attrs)
     {
