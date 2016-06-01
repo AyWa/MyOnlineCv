0b1311267637f35e610411c87aff8bfadbd26948
HMarc <marc.hurabielle@gmail.com>
2016-06-01 23:19:01 +0200

    form done: backend to do

--- Cv/js/directiveFooter.js
+++ Cv/js/directiveFooter.js
@@ -5,7 +5,18 @@ app.directive('myFooter',['myfactory',function(myfactory){
     templateUrl: 'template/footer.html',
     controller:function($scope){
       console.log("hello footer");
+      $scope.MyMessage={
+        name: "",
+        email: "",
+        message: "",
+      };
       $scope.myPers=myfactory.gotPers();
+      $scope.sendMessage = function(){
+        console.log("submit");
+        console.log($scope.MyMessage);
+        $scope.MyMessage={};
+        $scope.myForm.$setPristine();
+      }
     },
     link:function(scope,element,attrs)
     {
